6e6d42551df3425059843f849a930a2c339e9db8
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2025-02-20 17:48:42 +0100

    style: format files with black

--- tests/functionnal_test/test_functionnal_login_purchase_logout.py
+++ tests/functionnal_test/test_functionnal_login_purchase_logout.py
@@ -3,7 +3,10 @@ from urllib.parse import quote
 from selenium import webdriver
 
 from server import clubs, competitions
+
 # TODO: try to adapt functional test to updated data and not only data when app is started
+
+
 @pytest.mark.functional
 class TestFunctional:
     _base_url = "127.0.0.1:5000"
@@ -29,27 +32,46 @@ class TestFunctional:
 
             submit_button = driver.find_element("tag name", "button")
             submit_button.click()
-            assert f" Points available: {self._iron_temple["points"]}" in driver.page_source
+            assert (
+                f" Points available: {self._iron_temple["points"]}"
+                in driver.page_source
+            )
 
             # select competition
             # create link to follow to select the chosen competition
             # need to encode the link to avoid issues with special characters and spaces
-            link_value = f"/book/{self._future_competition['name']}/{self._iron_temple['name']}"
+            link_value = (
+                f"/book/{self._future_competition['name']}/{self._iron_temple['name']}"
+            )
             encoded_link = quote(link_value)
-            link_to_book_places = driver.find_element("xpath", f"//a[@href='{encoded_link}']")
+            link_to_book_places = driver.find_element(
+                "xpath", f"//a[@href='{encoded_link}']"
+            )
             link_to_book_places.click()
-            assert f"Places available: {self._future_competition["numberOfPlaces"]}" in driver.page_source
+            assert (
+                f"Places available: {self._future_competition["numberOfPlaces"]}"
+                in driver.page_source
+            )
 
             # book places
             places_input = driver.find_element("name", "places")
             places_input.send_keys(str(self._places_booked))
             submit_button = driver.find_element("tag name", "button")
             submit_button.click()
-            self._iron_temple["points"] = int(self._iron_temple["points"]) - self._places_booked
-            self._future_competition["numberOfPlaces"] = int(self._future_competition["numberOfPlaces"]) - self._places_booked
+            self._iron_temple["points"] = (
+                int(self._iron_temple["points"]) - self._places_booked
+            )
+            self._future_competition["numberOfPlaces"] = (
+                int(self._future_competition["numberOfPlaces"]) - self._places_booked
+            )
             assert "Great-booking complete!" in driver.page_source
-            assert f"Points available: {self._iron_temple["points"]}" in driver.page_source
-            assert f"Number of Places: {self._future_competition["numberOfPlaces"]}" in driver.page_source
+            assert (
+                f"Points available: {self._iron_temple["points"]}" in driver.page_source
+            )
+            assert (
+                f"Number of Places: {self._future_competition["numberOfPlaces"]}"
+                in driver.page_source
+            )
 
             # logout
             link_to_logout = driver.find_element("xpath", "//a[@href='/logout']")
